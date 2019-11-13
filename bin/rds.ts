626420fd3e4672feb19d370d5337df025d090260
natalie-elizabeth <natalie.elizabeth@andela.com>
2019-11-13 16:15:59 +0300

    Update bin file

--- bin/rds.ts
+++ bin/rds.ts
@@ -2,6 +2,18 @@
 import 'source-map-support/register';
 import cdk = require('@aws-cdk/core');
 import { RdsStack } from '../lib/rds-stack';
+import { DBTestVPCStack } from '../lib/vpc-stack';
 
 const app = new cdk.App();
-new RdsStack(app, 'RdsStack');
+const app_env = {
+  region: "us-east-1",
+  account: "488823795428"
+}
+
+new RdsStack(app, 'RdsStack', {
+  env: app_env
+});
+
+new DBTestVPCStack(app, 'DBTestVPCStack', {
+  env: app_env
+})
