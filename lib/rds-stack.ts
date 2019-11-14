b97a8929b4139ba203758453984c7216a705b9e9
natalie-elizabeth <natalie.elizabeth@andela.com>
2019-11-14 13:41:55 +0300

    - Enter Variables to be used in the rds file - Link the vpc - npm -i aws-rds
    Signed-off-by: natalie-elizabeth <natalie.elizabeth@andela.com>

--- lib/rds-stack.ts
+++ lib/rds-stack.ts
@@ -1,10 +1,33 @@
 import cdk = require('@aws-cdk/core');
+import ec2 = require('@aws-cdk/aws-ec2');
+import rds = require('@aws-cdk/aws-rds');
 
+// RDSSTackProps interface inherits from cdk.StackProps
+interface RDSStackProps extends cdk.StackProps {
+  vpc: ec2.IVpc;
+}
 export class RdsStack extends cdk.Stack {
+  private vpc: ec2.IVpc;
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+
     super(scope, id, props);
 
     // Context settings
-    const 
+    const masterUsername = this.node.tryGetContext("rds.masterUsername");
+    const databaseName = this.node.tryGetContext("rds.databaseName");
+    const dbInstanceIdentifier = this.node.tryGetContext("rds.dbInstanceIdentifier");
+    const availabilityZone = this.node.tryGetContext("rds.availabilityZone");
+    const backupRetentionPeriod = this.node.tryGetContext("rds.backupRentionPeriod");
+    const storageEncrypted = this.node.tryGetContext("rds.storageEncrypted");
+    const autoMinorVersionUpgrade = this.node.tryGetContext("rds.autoMinorVersionUpgrade");
+    const monitoringInterval = this.node.tryGetContext("rds.monitoringInterval");
+
+    if (props)
+      this.vpc = props.vpc;
+    else
+      this.vpc = ec2.Vpc.fromLookup(this, "DBTestVPC", {
+        vpcName: "DBTestVPCStack/DBTestVPC"
+      });
+
   }
 }
